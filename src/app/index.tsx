fd67d47949a155ae82676c0b4ab29b3ac571ffa3
Jaiten Made <jaiten.thomas@gmail.com>
2025-09-18 20:13:46 +1000

    feat: add CssBaseline for consistent styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "../lib/mui/theme";
 import MainDrawer from "./components/MainDrawer";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <MainDrawer />
     </ThemeProvider>
   );
