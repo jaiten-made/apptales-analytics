cb091d7919e46e0e41341330cab1f44f63bfe31c
Jaiten Made <jaiten.thomas@gmail.com>
2025-10-02 21:33:25 +1000

    fix: correct indentation in theme palette definition in theme.ts

--- apps/app/src/lib/mui/theme.ts
+++ apps/app/src/lib/mui/theme.ts
@@ -1,6 +1,11 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFBF00",
+    },
+  },
   mixins: {
     drawer: {
       minWidth: 300,
