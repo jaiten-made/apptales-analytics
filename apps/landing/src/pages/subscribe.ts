07b861de275eb9be41fad3c50b884a605e4aec93
Jaiten Made <jaiten.thomas@gmail.com>
2025-10-07 19:02:19 +1000

    feat: add POST API route for handling subscriptions refactor: standardize import formatting and indentation in index.astro

---
+++ apps/landing/src/pages/subscribe.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = ({ request }) => {
+  return new Response(
+    JSON.stringify({
+      message: "This was a POST!",
+    })
+  );
+};
