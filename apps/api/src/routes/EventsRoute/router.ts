8e1a3f0e9b9d2981ebaa6830945537ef020d80a0
Jaiten Made <jaiten.thomas@gmail.com>
2025-10-30 20:54:54 +1000

    feat(Event): add GET /events endpoint to retrieve all events with associated data

--- apps/api/src/routes/EventsRoute/router.ts
+++ apps/api/src/routes/EventsRoute/router.ts
@@ -3,6 +3,27 @@ import { prisma } from "../../lib/prisma/client";
 
 const router = express.Router();
 
+// @route  GET /events
+// @desc   Get all events
+router.get("/", async (req, res) => {
+  try {
+    const events = await prisma.event.findMany({
+      include: {
+        data: true,
+      },
+    });
+    res.json(events);
+  } catch (error) {
+    let message = "";
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    res.status(500).json({
+      message,
+    });
+  }
+});
+
 // POST /events to save click events
 router.post("/", async (req: Request, res: Response) => {
   const { type, data } = req.body;
