f0e9b2c08dcb39462d28dcb161afec59e0899897
Jaiten Made <jaiten.thomas@gmail.com>
2025-11-25 21:25:44 +1000

    feat(auth): update session route to '/verify' for clarity

--- apps/api/src/routes/AuthRoute/Session/router.ts
+++ apps/api/src/routes/AuthRoute/Session/router.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/verify", async (req, res, next) => {
   try {
     const token = req.cookies?.session;
     jwt.verify(token, process.env.JWT_SECRET!) as {
