fb147bd61c665aa30fb0dc4bcef545d56d21cb61
Jaiten Made <jaiten.thomas@gmail.com>
2025-12-24 16:53:38 +1000

    fix: Allow console logs in production by setting drop_console to false

--- apps/tracker/vite.config.ts
+++ apps/tracker/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
       minify: "terser",
       terserOptions: {
         compress: {
-          drop_console: true,
+          drop_console: false,
         },
         format: {
           comments: false,
