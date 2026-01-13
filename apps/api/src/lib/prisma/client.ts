bc3462604f8fed4411feab04fe44236f0ff98461
Jaiten Made <jaiten.thomas@gmail.com>
2026-01-13 20:13:18 +1000

    refactor: simplify PrismaClient logging configuration for production

--- apps/api/src/lib/prisma/client.ts
+++ apps/api/src/lib/prisma/client.ts
@@ -2,10 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const createPrisma = () =>
   new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+    log: ["error"],
   });
 
 type PrismaWithEvents = ReturnType<typeof createPrisma>;
